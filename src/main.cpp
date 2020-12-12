214458c542e9f77ce8d83362e29e7d791ce3aab5
Saggre <sakri.koskimies@hotmail.com>
2020-12-12 15:46:58 +0200

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@ int main() {
     twochains::Block genesis_block(0, "", twochains::Block::GetCurrentTimestamp(), twochains::BlockPayload(data));
     twochains::ChainManager chain(genesis_block);
 
+    char lorem[256] = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    chain.GenerateNextBlock(twochains::BlockPayload(lorem));
+
     std::cout << chain.GetLatestBlock()->GetPayload().data_ << '\n';
     std::cout << chain.GetLatestBlock()->GetHash() << '\n';
 
